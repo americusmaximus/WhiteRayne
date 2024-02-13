f51af8dae0bacaba16059ece820f0b527f0c59e6
Americus Maximus <americancodemax@gmail.com>
2024-02-13 08:39:41 -0500

    Improvements.

--- Source/VideoPlayer/Player.cxx
+++ Source/VideoPlayer/Player.cxx
@@ -64,7 +64,7 @@ void StringFormat(char* buffer, const char* format, ...)
 {
     va_list args;
     va_start(args, format);
-    vsnprintf_s(buffer, MAX_BUFFER_SIZE, format, args);
+    vsprintf_s(buffer, MAX_BUFFER_SIZE, format, args);
     va_end(args);
 }
 
